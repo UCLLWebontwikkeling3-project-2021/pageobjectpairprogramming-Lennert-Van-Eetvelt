70329f3f7bd03e102a8f0da8e178ea79ef622d03
Lennert Van Eetvelt <lennert.vaneetvelt@gmail.com>
2020-12-26 22:01:24 +0100

    Add files via upload

---
+++ PageObjectTests/LogoutPage.java
@@ -0,0 +1,12 @@
+package PageObjectTests;
+
+import org.openqa.selenium.WebDriver;
+
+public class LogoutPage extends Page {
+
+    public LogoutPage (WebDriver driver) {
+        super(driver);
+        this.driver.get(getPath()+"?command=Logout");
+    }
+
+}
